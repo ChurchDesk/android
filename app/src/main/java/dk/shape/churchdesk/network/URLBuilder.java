27cd1508200c930476e6cf69e530624f1ab7ff63
Steffen Karlsson <steffenkarlsson@steffenkarlssons-MacBook-Pro.local>
2015-03-23 09:28:15 +0100

    CHD url builder specific and initial URLUtils

---
+++ app/src/main/java/dk/shape/churchdesk/network/URLBuilder.java
@@ -0,0 +1,40 @@
+package dk.shape.churchdesk.network;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by steffenkarlsson on 23/12/14.
+ */
+public class URLBuilder {
+
+    private boolean mDebug = true;
+    private final static String HOST_NAME = "http://api.churchdesk.com/";
+    private final static String DEBUG_HOST_NAME = "http://private-anon-83c43a3ef-churchdeskapi.apiary-mock.com/";
+    private String mSubdomain = "";
+    private List<NameValuePair> mParameters = new ArrayList<>();
+
+    public URLBuilder setDebug() {
+        this.mDebug = true;
+        return this;
+    }
+
+    public URLBuilder subdomain(String subdomain) {
+        this.mSubdomain += subdomain;
+        return this;
+    }
+
+    public URLBuilder addParameter(String key, String value) {
+        mParameters.add(new BasicNameValuePair(key, value));
+        return this;
+    }
+
+    public String build() {
+        return (mDebug ? DEBUG_HOST_NAME : HOST_NAME) + mSubdomain
+                + (mParameters.size() > 0 ? "?" + URLEncodedUtils.format(mParameters, "utf-8") : "");
+    }
+}
---
+++ app/src/main/java/dk/shape/churchdesk/request/URLUtils.java
@@ -0,0 +1,40 @@
+package dk.shape.churchdesk.request;
+
+import android.content.Context;
+
+import dk.shape.churchdesk.R;
+import dk.shape.churchdesk.network.URLBuilder;
+
+/**
+ * Created by steffenkarlsson on 22/12/14.
+ */
+public class URLUtils {
+
+    private static String token = "";
+
+    public static void setAccessToken(String token) {
+        URLUtils.token = token;
+    }
+
+    private static URLBuilder apiBuilder(String target) {
+        return new URLBuilder().subdomain("api/v1/" + target);
+    }
+
+    private static URLBuilder oauthBuilder(String target) {
+        return new URLBuilder().subdomain("oauth/v2/" + target);
+    }
+
+    private static URLBuilder authenticatedApiBuilder(String target) {
+        return apiBuilder(target).addParameter("access_token", token);
+    }
+
+    public static String getLoginUrl(Context context, String username, String password) {
+        return oauthBuilder("token")
+                .addParameter("client_id", context.getString(R.string.api_client_id))
+                .addParameter("client_secret", context.getString(R.string.api_client_secret))
+                .addParameter("grant_type", "password")
+                .addParameter("username", username)
+                .addParameter("password", password)
+                .build();
+    }
+}
