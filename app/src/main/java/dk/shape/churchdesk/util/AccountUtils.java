632af2d8a3c509ab660c64995816b996946129da
Steffen Karlsson <steffenkarlsson@steffenkarlssons-MacBook-Pro.local>
2015-03-23 09:50:00 +0100

    rewriting accountutils to handle AccessToken

--- app/src/main/java/dk/shape/churchdesk/util/AccountUtils.java
+++ app/src/main/java/dk/shape/churchdesk/util/AccountUtils.java
@@ -5,13 +5,16 @@ import android.content.SharedPreferences;
 import android.util.Pair;
 
 import dk.shape.churchdesk.R;
+import dk.shape.churchdesk.entity.AccessToken;
+import dk.shape.churchdesk.network.ParserException;
+
+import static dk.shape.churchdesk.network.RequestUtils.parse;
 
 /**
  * Created by steffenkarlsson on 30/01/15.
  */
 public class AccountUtils {
 
-    private static final String KEY_EMAIL = "KEY_EMAIL";
     private static final String KEY_TOKEN = "KEY_TOKEN";
 
     private static AccountUtils ourInstance = null;
@@ -28,29 +31,30 @@ public class AccountUtils {
                 context.getString(R.string.app_name), Context.MODE_PRIVATE);
     }
 
-    public void saveAccount(String email, String token) {
+    public void saveToken(AccessToken token) {
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(KEY_EMAIL, email);
-        editor.putString(KEY_TOKEN, token);
+        editor.putString(KEY_TOKEN, parse(token));
         editor.apply();
     }
 
     public boolean containsAccount() {
-        return sharedPreferences.contains(KEY_EMAIL) &&
-                sharedPreferences.contains(KEY_TOKEN);
+        return sharedPreferences.contains(KEY_TOKEN);
     }
 
     public void clear() {
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.remove(KEY_EMAIL);
         editor.remove(KEY_TOKEN);
         editor.apply();
     }
 
-    public Pair<String, String> getAccount() {
-        if (containsAccount()) {
-            return new Pair<>(sharedPreferences.getString(KEY_EMAIL, ""),
-                    sharedPreferences.getString(KEY_TOKEN, ""));
+    public AccessToken getAccount() {
+        try {
+            if (containsAccount()) {
+                String accessToken = sharedPreferences.getString(KEY_TOKEN, "");
+                if (accessToken != null && !accessToken.isEmpty())
+                    return parse(AccessToken.class, accessToken);
+            }
+        } catch (ParserException ignore) {
         }
         return null;
     }
