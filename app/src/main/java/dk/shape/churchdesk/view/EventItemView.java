b95e3cce4958fb5ab71bd05a01a38bebed695035
Steffen Karlsson <steffenkarlsson@steffenkarlssons-MacBook-Pro.local>
2015-03-31 12:11:14 +0200

    single event item, missing correct time

---
+++ app/src/main/java/dk/shape/churchdesk/view/EventItemView.java
@@ -0,0 +1,55 @@
+package dk.shape.churchdesk.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import butterknife.InjectView;
+import de.hdodenhof.circleimageview.CircleImageView;
+import dk.shape.churchdesk.R;
+import dk.shape.churchdesk.widget.CustomTextView;
+
+/**
+ * Created by steffenkarlsson on 24/03/15.
+ */
+public class EventItemView extends BaseFrameLayout {
+
+    @InjectView(R.id.event_color)
+    public View mEventColor;
+
+    @InjectView(R.id.event_title)
+    public CustomTextView mEventTitle;
+
+    @InjectView(R.id.event_time)
+    public CustomTextView mEventTime;
+
+    @InjectView(R.id.event_site)
+    public CustomTextView mEventSite;
+
+    @InjectView(R.id.event_location)
+    protected CustomTextView mEventLocation;
+
+    @InjectView(R.id.event_location_wrapper)
+    protected LinearLayout mEventLocationWrapper;
+
+    public EventItemView(Context context) {
+        super(context);
+    }
+
+    public EventItemView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.part_event_item;
+    }
+
+    public void setLocation(String location) {
+        if (location == null || location.isEmpty())
+            mEventLocationWrapper.setVisibility(INVISIBLE);
+        else
+            mEventLocation.setText(location);
+    }
+}
---
+++ app/src/main/java/dk/shape/churchdesk/viewmodel/EventItemViewModel.java
@@ -0,0 +1,70 @@
+package dk.shape.churchdesk.viewmodel;
+
+import android.text.format.DateUtils;
+import android.view.View;
+
+import java.util.List;
+
+import dk.shape.churchdesk.R;
+import dk.shape.churchdesk.entity.Event;
+import dk.shape.churchdesk.entity.Site;
+import dk.shape.churchdesk.entity.User;
+import dk.shape.churchdesk.entity.resources.Category;
+import dk.shape.churchdesk.util.DatabaseUtils;
+import dk.shape.churchdesk.view.EventItemView;
+import dk.shape.library.viewmodel.ViewModel;
+
+/**
+ * Created by steffenkarlsson on 24/03/15.
+ */
+public class EventItemViewModel extends ViewModel<EventItemView> {
+
+    public interface OnEventClickListener {
+        void onClick(Event event);
+    }
+
+    private final OnEventClickListener mListener;
+    private final User mCurrentUser;
+    private final Event mEvent;
+
+    public EventItemViewModel(Event event, User currentUser,
+                              OnEventClickListener listener) {
+        this.mEvent = event;
+        this.mCurrentUser = currentUser;
+        this.mListener = listener;
+    }
+
+    @Override
+    public void bind(EventItemView eventItemView) {
+        DatabaseUtils db = DatabaseUtils.getInstance();
+
+        Site site = null;
+        if (!mCurrentUser.isSingleUser())
+            site = mCurrentUser.getSiteById(mEvent.mSiteUrl);
+        eventItemView.mEventSite.setText(site != null ? site.mSiteName : "");
+
+        eventItemView.mEventTitle.setText(mEvent.mTitle);
+
+        List<Category> categories = db.getCategoriesBySiteId(mEvent.mSiteUrl,
+                String.valueOf(mEvent.mCategories.get(0)));
+        if (categories == null || categories.isEmpty())
+            eventItemView.mEventColor.setVisibility(View.GONE);
+        else
+            eventItemView.mEventColor.setBackgroundColor(categories.get(0).getColor());
+
+        eventItemView.setLocation(mEvent.mLocation);
+
+        eventItemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onClick(mEvent);
+            }
+        });
+
+        if (mEvent.isAllDay)
+            eventItemView.mEventTime.setText(R.string.all_day);
+        else
+            eventItemView.mEventTime.setText("");
+
+    }
+}
