a45ec76dbc8bfcc8c35a720cf64442c3be3d55ef
Steffen Karlsson <steffenkarlsson@steffenkarlssons-MacBook-Pro.local>
2015-04-07 10:33:25 +0200

    invitations viewmodel and appearance, missing correct time

---
+++ app/src/main/java/dk/shape/churchdesk/viewmodel/InvitationItemViewModel.java
@@ -0,0 +1,62 @@
+package dk.shape.churchdesk.viewmodel;
+
+import android.text.format.DateUtils;
+import android.view.View;
+
+import java.util.Calendar;
+import java.util.List;
+
+import dk.shape.churchdesk.R;
+import dk.shape.churchdesk.entity.Event;
+import dk.shape.churchdesk.entity.Site;
+import dk.shape.churchdesk.entity.User;
+import dk.shape.churchdesk.entity.resources.Category;
+import dk.shape.churchdesk.entity.resources.OtherUser;
+import dk.shape.churchdesk.util.DatabaseUtils;
+import dk.shape.churchdesk.view.EventItemView;
+import dk.shape.churchdesk.view.InvitationItemView;
+import dk.shape.library.viewmodel.ViewModel;
+
+/**
+ * Created by steffenkarlsson on 24/03/15.
+ */
+public class InvitationItemViewModel extends ViewModel<InvitationItemView> {
+
+    private final User mCurrentUser;
+    private final Event mInvitation;
+
+    public InvitationItemViewModel(Event invitation, User currentUser) {
+        this.mInvitation = invitation;
+        this.mCurrentUser = currentUser;
+    }
+
+    @Override
+    public void bind(InvitationItemView invitationItemView) {
+        DatabaseUtils db = DatabaseUtils.getInstance();
+
+        Site site = null;
+        if (!mCurrentUser.isSingleUser())
+            site = mCurrentUser.getSiteById(mInvitation.mSiteUrl);
+        invitationItemView.mEventSite.setText(site != null ? site.mSiteName : "");
+
+        invitationItemView.mEventTitle.setText(mInvitation.mTitle);
+
+        List<Category> categories = db.getCategoriesBySiteId(mInvitation.mSiteUrl,
+                String.valueOf(mInvitation.mCategories.get(0)));
+        if (categories == null || categories.isEmpty())
+            invitationItemView.mEventColor.setVisibility(View.GONE);
+        else
+            invitationItemView.mEventColor.setBackgroundColor(categories.get(0).getColor());
+
+        invitationItemView.setLocation(mInvitation.mLocation);
+
+        invitationItemView.mCreatedAgo.setText(DateUtils.getRelativeTimeSpanString(
+                mInvitation.mChanged.getTime(), System.currentTimeMillis(),
+                DateUtils.MINUTE_IN_MILLIS));
+
+        OtherUser user = db.getUserById(mInvitation.mInvitedBy);
+        invitationItemView.mByWhoTitle.setText(user != null
+                ? invitationItemView.getContext().getString(R.string.invited_by, user.mName)
+                : "");
+    }
+}
---
+++ app/src/main/java/dk/shape/churchdesk/viewmodel/InvitationsViewModel.java
@@ -0,0 +1,77 @@
+package dk.shape.churchdesk.viewmodel;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+import dk.shape.churchdesk.R;
+import dk.shape.churchdesk.entity.Event;
+import dk.shape.churchdesk.entity.User;
+import dk.shape.churchdesk.view.InvitationItemView;
+import dk.shape.churchdesk.view.RefreshView;
+
+/**
+ * Created by steffenkarlsson on 24/03/15.
+ */
+public class InvitationsViewModel extends BaseDashboardViewModel<RefreshView, List<Event>> {
+
+    private final User mCurrentUser;
+
+    private List<Event> mEventInvites;
+    private Context mContext;
+
+    public InvitationsViewModel(User currentUser, OnRefreshData onRefreshData) {
+        super(onRefreshData);
+        this.mCurrentUser = currentUser;
+    }
+
+    @Override
+    public void extBind(RefreshView view, List<Event> data) {
+        this.mEventInvites = data;
+        super.extBind(view, data);
+    }
+
+    @Override
+    public int getEmptyRes() {
+        return R.string.no_invitations;
+    }
+
+    @Override
+    public void bind(RefreshView refreshView) {
+        mContext = refreshView.getContext();
+        refreshView.swipeContainer.setRefreshing(false);
+        refreshView.swipeContainer.setColorSchemeResources(R.color.foreground_blue);
+        refreshView.swipeContainer.setOnRefreshListener(mOnRefreshListener);
+        refreshView.mMessageList.setAdapter(new InvitesAdapter());
+    }
+
+    private class InvitesAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return mEventInvites.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            InvitationItemView view = new InvitationItemView(mContext);
+            InvitationItemViewModel viewModel = new InvitationItemViewModel(
+                    mEventInvites.get(position), mCurrentUser);
+            viewModel.bind(view);
+            return view;
+        }
+    }
+}
