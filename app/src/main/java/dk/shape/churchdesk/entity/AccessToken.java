5943d60e1d8d24f9175b738f437bcd02798b7339
Steffen Karlsson <steffen.karlsson@gmail.com>
2015-05-20 12:50:13 +0200

    add expiration checks

--- app/src/main/java/dk/shape/churchdesk/entity/AccessToken.java
+++ app/src/main/java/dk/shape/churchdesk/entity/AccessToken.java
@@ -4,6 +4,10 @@ import com.google.gson.annotations.SerializedName;
 
 import org.parceler.Parcel;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by steffenkarlsson on 23/03/15.
  */
@@ -20,7 +24,18 @@ public class AccessToken {
     @SerializedName("expires_in")
     protected int mExpiredIn;
 
+    protected Calendar mExpiresAt;
+
     public String getRefreshToken() {
         return mRefreshToken;
+    }
+
+    public void calculateExpirationDate() {
+        mExpiresAt = Calendar.getInstance();
+        mExpiresAt.add(Calendar.SECOND, mExpiredIn - (int) TimeUnit.MINUTES.toSeconds(5));
+    }
+
+    public boolean isExpired() {
+        return mExpiresAt.before(Calendar.getInstance());
     }
 }
