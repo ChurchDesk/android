b41c9e68d754b22e8799384128feaf49c1fc29e9
Steffen Karlsson <steffen.karlsson@gmail.com>
2015-06-05 17:54:17 +0200

    rewritten due to calendar

--- app/src/main/java/dk/shape/churchdesk/request/GetEvents.java
+++ app/src/main/java/dk/shape/churchdesk/request/GetEvents.java
@@ -3,17 +3,20 @@ package dk.shape.churchdesk.request;
 import com.google.gson.reflect.TypeToken;
 
 import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import dk.shape.churchdesk.entity.Event;
 import dk.shape.churchdesk.network.GetRequest;
 import dk.shape.churchdesk.network.ParserException;
 
 import static dk.shape.churchdesk.network.RequestUtils.parse;
+import static dk.shape.churchdesk.util.MapUtils.merge;
 
 /**
  * Created by steffenkarlsson on 31/03/15.
  */
-public class GetEvents extends GetRequest<List<Event>> {
+public class GetEvents extends GetRequest<SortedMap<Long, List<Event>>> {
 
     protected GetEvents(String url) {
         super(url);
@@ -24,7 +27,10 @@ public class GetEvents extends GetRequest<List<Event>> {
     }
 
     @Override
-    protected List<Event> parseHttpResponseBody(String body) throws ParserException {
-        return parse(new TypeToken<List<Event>>() {}, body);
+    protected SortedMap<Long, List<Event>> parseHttpResponseBody(String body) throws ParserException {
+        SortedMap<Long, List<Event>> eventsMap = new TreeMap<>();
+        for (Event event : parse(new TypeToken<List<Event>>() {}, body))
+            eventsMap = merge(eventsMap, event.convertToMultipleEvents());
+        return eventsMap;
     }
 }
