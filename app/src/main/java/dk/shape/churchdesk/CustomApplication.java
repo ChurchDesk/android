7624dcb7255558bd86197245401d226420bfe541
Steffen Karlsson <steffen.karlsson@gmail.com>
2015-06-11 12:50:18 +0200

    custom application

---
+++ app/src/main/java/dk/shape/churchdesk/CustomApplication.java
@@ -0,0 +1,11 @@
+package dk.shape.churchdesk;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by steffenkarlsson on 6/11/15.
+ */
+public class CustomApplication extends MultiDexApplication {
+
+    public boolean hasSendRegistrationId = false;
+}
